8bc4e4474693a6967f5fdea8d4d5b6951524d40d
ebfaler <74653573+ebfaler@users.noreply.github.com>
2022-06-25 16:21:54 +0200

    working on form update

--- routes/index.js
+++ routes/index.js
@@ -25,7 +25,7 @@ router.get('/', asyncHandler(async (req, res) => {
 router.get('/books', asyncHandler(async (req, res) => {
 
   const books = await Book.findAll();
-  res.render('index', { books, title: "Cataloge of Books" });
+  res.render('index', { books, title: "Catalogue of Books" });
 
   // console.log(books);
   // console.log(books.map(book => book.toJSON()));
@@ -38,7 +38,7 @@ router.get('/books', asyncHandler(async (req, res) => {
 //shows the create new book form
 
 router.get('/books/new', asyncHandler(async (req, res) => {
-  res.render('new-book', { title: "Add a new book" });
+  res.render('new-book', { book: {},title: "Add a new book" });
 })
 
 );
@@ -46,14 +46,53 @@ router.get('/books/new', asyncHandler(async (req, res) => {
 /* POST new book form. */
 //posts a new book onto the database
 
+router.post('/books/new', asyncHandler(async (req, res) => {
+  const book = await Book.create(req.body);
+  res.redirect("/books/" + book.id);
 
-/* GET book detail form. */
+})
 
-/*POST updates of book info into the database. */
+);
+
+
+/* GET book update form. */
+
+router.get('/books/:id', asyncHandler(async (req, res) => {
+  console.log("displaying book form");
+
+  res.render('update-book', {book: {}, title: "Added book to database" });
+
+
+})
+
+);
+
+/*POST update of book info into the database. */
+
+router.post('/books/:id', asyncHandler(async (req, res) => {
+  console.log("posting book form");
+
+  const book = await Book.findByPk(req.params.id);
+  await book.update(req.body);
+  
+  res.redirect("/books/" + book.id);
+  //add text to say book has been updated
+
+})
+
+);
+
 
 /*POST delete a book. */
+// /books/:id/delete
 
 
+router.post('/books/:id/delete', asyncHandler(async (req, res) => {
+  console.log("deleting a book");
+  
+})
+
+);
 
 
 
